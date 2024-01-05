0cb92a54246f66b5b3aed9e1273d5a8fd1d6c4fb
Mark Hsu <mark.g.hsu@gmail.com>
2024-01-04 19:54:48 -0800

    Feat: Add To, From, Like fields Likes are controlled via localStorage -- clearing localstorage will reset your ability to like endorsements. We could improve this via saving a uuid in the database, or requiring real user logins.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // FIREBASE SETUP
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
-import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove, runTransaction } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
 
 const firebaseConfig = {
 	databaseURL: "https://scrimba-champion-default-rtdb.firebaseio.com/"
@@ -12,12 +12,23 @@ const database = getDatabase(app);
 const endorsementsInDB = ref(database,"endorsements");
 
 const endorsementInputEl = document.getElementById("endorsement-input");
+const fromInputEl = document.getElementById("from-input");
+const toInputEl = document.getElementById("to-input");
 const publishBtnEl = document.getElementById("publish-btn");
 const endorsementListEl = document.getElementById("endorsement-list");
 
+const myLikes = JSON.parse(localStorage.getItem("likes")) || {};
+
 publishBtnEl.addEventListener("click", () => {
-	push(endorsementsInDB,endorsementInputEl.value);
+	const endorsement = {};
+	endorsement['message'] = _strip(endorsementInputEl.value);
+	endorsement['from'] = _strip(fromInputEl.value);
+	endorsement['to'] = _strip(toInputEl.value);
+	endorsement['likes'] = 0;
+	push(endorsementsInDB,endorsement);
 	endorsementInputEl.value = '';
+	fromInputEl.value = '';
+	toInputEl.value = '';
 });
 
 onValue(endorsementsInDB,(snapshot) => {
@@ -27,7 +38,6 @@ onValue(endorsementsInDB,(snapshot) => {
 		const endorsements = Object.entries(snapshot.val());
 		console.log(endorsements,"xxxx")
 		endorsements.forEach((endorsement) => {
-			console.log(endorsement);
 			renderEndorsement(...endorsement);
 		});
 	}
@@ -37,9 +47,71 @@ function clearEndorsementList() {
 	endorsementListEl.innerHTML = '';
 }
 
-function renderEndorsement(endorsementID, endorsementValue) {
+function renderEndorsement(endorsementID, endorsement) {
 	const listItem = document.createElement('li');
-	listItem.textContent = endorsementValue;
+
+	const itemTo = document.createElement('p');
+	itemTo.textContent = `To ${endorsement['to']}`;
+	itemTo.classList.add('endorsement-label');
+
+	const itemMessage = document.createElement('p');
+	itemMessage.textContent = endorsement['message'];
+	itemMessage.classList.add('endorsement-message');
+
+	const itemFrom = document.createElement('p');
+	itemFrom.textContent = `From ${endorsement['from']}`;
+	itemFrom.classList.add('endorsement-label');
+
+	const itemLikes = document.createElement('p');
+	if(myLikes[endorsementID]) {
+		itemLikes.textContent = `❤️ ${endorsement['likes']}`;
+	} else {
+		itemLikes.textContent = `❤ ${endorsement['likes']}`;
+	}
+	itemLikes.classList.add('likes');
+	itemLikes.addEventListener("click",() => {
+		toggleLike(endorsementID);
+	})
+
+	const itemRow = document.createElement('div');
+	itemRow.classList.add('flex-row');
+	itemRow.appendChild(itemFrom);
+	itemRow.appendChild(itemLikes);
+
 	listItem.dataset.key = endorsementID;
+	listItem.classList.add('endorsement');
+	listItem.appendChild(itemTo);
+	listItem.appendChild(itemMessage);
+	listItem.appendChild(itemRow);
+
 	endorsementListEl.appendChild(listItem);
+}
+
+function _strip(html){
+	let doc = new DOMParser().parseFromString(html, 'text/html');
+	return doc.body.textContent || '';
+}
+
+function toggleLike(endorsementID) {
+	const endorsementRef = ref(database, `/endorsements/${endorsementID}`);
+	if(myLikes[endorsementID]){
+		//unliking
+		delete myLikes[endorsementID];
+		localStorage.setItem("likes",JSON.stringify(myLikes));
+		runTransaction(endorsementRef, (endorsement) => {
+			if (endorsement) {
+				endorsement.likes--;
+			}
+			return endorsement;
+		});
+	} else {
+		myLikes[endorsementID] = true;
+		localStorage.setItem("likes",JSON.stringify(myLikes));
+		runTransaction(endorsementRef, (endorsement) => {
+			if (endorsement) {
+				endorsement.likes++;
+			}
+			return endorsement;
+		});
+	}
 }
